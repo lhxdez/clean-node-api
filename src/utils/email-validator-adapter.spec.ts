6eb270323e9e397850ec90ff1149a261a547e320
LucasGomes98 <lucas.gomes@startse.com>
2022-04-12 10:09:10 -0300

    feat: terminar

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,15 @@
 import { EmailValidatorAdapter } from './email-validator'
 
 describe('EmailValidator Adapter', () => {
-  test('Should return false igf validator returns false', () => {
+  test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
